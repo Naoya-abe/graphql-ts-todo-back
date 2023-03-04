35459b93d733161c65600ddfd10782e35e0d8cde
阿部直也 <n.abe@gemcook.com>
2023-03-04 11:44:11 +0900

    CORSの設定

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // CORSの設定
+  app.enableCors({ credentials: true, origin: ['http://localhost:3000'] });
   await app.listen(8080);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
